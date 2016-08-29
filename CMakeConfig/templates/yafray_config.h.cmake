9d51966c30f94e5a419e9654c4206da702fcebf7
inferno@hmstr <inferrna@gmail.com>
2016-08-29 15:24:22 +0800

    cmake pyver fix

--- CMakeConfig/templates/yafray_config.h.cmake
+++ CMakeConfig/templates/yafray_config.h.cmake
@@ -3,20 +3,23 @@
 #define Y_CONFIG_H
 #include "yafray_constants.h"
 #include <core_api/color_console.h>
-#include <core_api/session.h>
+#include <core_api/console_verbosity.h>
 
 #define MIN_RAYDIST @YAF_MIN_RAY_DIST@
 #define YAF_SHADOW_BIAS @YAF_SHADOW_BIAS@
 
-#define Y_DEBUG yafLog.out(VL_DEBUG)
-#define Y_VERBOSE yafLog.out(VL_VERBOSE)
-#define Y_INFO yafLog.out(VL_INFO)
-#define Y_PARAMS yafLog.out(VL_PARAMS)
-#define Y_WARNING yafLog.out(VL_WARNING)
-#define Y_ERROR yafLog.out(VL_ERROR)
+#define Y_INFO yafout.info() << setColor(Green) << "INFO: " << setColor()
+#define Y_WARNING yafout.warning() << setColor(Yellow) << "WARNING: " << setColor()
+#define Y_ERROR yafout.error() << setColor(Red) << "ERROR: " << setColor()
+#define Y_LOG yafout.error() << setColor(Cyan) << "LOG: " << setColor()
 #define yendl std::endl
 
 #cmakedefine HAVE_UNISTD_H 1
 
+__BEGIN_YAFRAY
+typedef float CFLOAT;
+typedef float GFLOAT;
+typedef float PFLOAT;
+__END_YAFRAY
 #endif
 
